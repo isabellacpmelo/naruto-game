307398697b05b1a083884b9a684316ce943ae39c
isabellacpmelo <isacpmelo@gmail.com>
2023-07-24 08:52:03 -0300

    feat: adding sakura animation

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,28 +1,12 @@
 <!-- @format -->
 <template>
-  <div class="q-pa-md">
-    <q-card flat bordered>
-      <q-item>
-        <q-item-section avatar>
-          <q-skeleton type="QAvatar" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>
-            <q-skeleton type="text" />
-          </q-item-label>
-          <q-item-label caption>
-            <q-skeleton type="text" width="80%" />
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-item>
-        <q-item-section avatar />
-        <q-item-section>
-          <q-skeleton height="50px" class="mb-2" />
-          <q-skeleton height="50px" class="mb-2" />
-          <q-skeleton height="50px" />
-        </q-item-section>
-      </q-item>
-    </q-card>
+  <div class="game-board">
+    <div>
+      <img
+        src="https://github.com/isabellacpmelo/naruto-game/blob/main/src/assets/images/sakura.gif?raw=true"
+        alt="Sakura Fighting"
+        class="sakura"
+      >
+    </div>
   </div>
 </template>
