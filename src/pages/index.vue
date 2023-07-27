92374e96c0e45f3aeff1e5af499d71c8dcd736ee
isabellacpmelo <isacpmelo@gmail.com>
2023-07-26 22:37:10 -0300

    fix: updating configs

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,11 @@
 <!-- @format -->
-<script setup></script>
+<script setup>
+// when keydown is pressed, naruto will recive the class .jump
+function narutoJump() {
+  console.warn('naruto jumping')
+  // document.querySelector(".naruto").classList.add("jumppnpp");
+}
+</script>
 
 <template>
   <div class="game-board">
@@ -13,6 +19,7 @@
         src="https://github.com/isabellacpmelo/naruto-game/blob/main/src/assets/images/naruto-running.gif?raw=true"
         alt="Naruto Running"
         class="naruto"
+        @keyup="narutoJump()"
       >
     </div>
   </div>
