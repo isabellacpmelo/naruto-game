63252751bd1808234413e9a1b7e4e100df66bef3
isabellacpmelo <isacpmelo@gmail.com>
2023-07-26 21:57:28 -0300

    fix: resizing naruto gif

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,6 @@
 <!-- @format -->
+<script setup></script>
+
 <template>
   <div class="game-board">
     <div>
@@ -6,6 +8,11 @@
         src="https://github.com/isabellacpmelo/naruto-game/blob/main/src/assets/images/sakura.gif?raw=true"
         alt="Sakura Fighting"
         class="sakura"
+      >
+      <img
+        src="https://github.com/isabellacpmelo/naruto-game/blob/main/src/assets/images/naruto-running.gif?raw=true"
+        alt="Naruto Running"
+        class="naruto"
       >
     </div>
   </div>
